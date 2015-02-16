33dbdb54422f79891a0e422845ba5e2f43a6ff08
=Alessio <alessio.piergiacomi@gmail.com>
2015-02-17 00:02:13 +0100

    creazione tabella

--- backbone-app.js
+++ backbone-app.js
@@ -16,7 +16,7 @@
 
   var ListView = Backbone.View.extend({
       
-    el: $('div#right'), // attaches `this.el` to an existing element.
+    el: $('div#left'), // attaches `this.el` to an existing element.
       
     // `initialize()`: Automatically called upon instantiation. Where you make all types of bindings, _excluding_ UI events, such as clicks, etc.
     initialize: function(){
@@ -39,18 +39,63 @@
         //var self = this;
         
         $(this.el).append("<button id='add'>Add list item</button>");
-        $(this.el).append("<ul></ul>");
+        $(this.el).append("<table class=\"table table-striped table-hover \">"
+                +"<thead>"
+                    +"<tr>"
+                        +"<th>#</th>"
+                        +"<th>Concept</th>"
+                        +"<th>Mandatory</th>"
+                        +"<th>Random</th>"
+                        +"<th>Disabled</th>"
+                        +"<th>Remove</th>"
+                    +"</tr>"
+                +"</thead><tbody></tbody></table>");
     }
       
     , addItem: function(){
         
-      this.counter++;
+        this.counter++;
         item =  new Item();
-        item.set({ concept : prompt("ahoao"), choice: "random" });
+        item.set({ concept : prompt("ahoao"), choice: "random", counter: this.counter });
         this.collection.add(item);
                 
         
-      $('ul', this.el).append("<li>hello world"+this.counter+ item.get('concept') + item.get('choice') + " </li>");
+      $('tbody', this.el).append(
+            '<tr id="concept1">' 
+                        +'<td>' +  this.counter + '</td>'
+                        +'<td>'
+                            +'<div class="form-group">'
+                                +'<div class="form-control-wrapper"><input value="' + item.get('concept') +'" id="concept' +  item.get('counter') + '_text" class="form-control"  type="text"><span class="material-input"></span></div>'
+                            +'</div>'
+                        +'</td>'
+                        +'<td>'
+                            +'<div class="radio radio-primary">'
+                                +'<label>'
+                                    +'<input type="radio" name="concept' + item.get('counter') + '_choice" id="concept' + item.get('counter') + '_choice" value="mandatory">'
+                                    +'<span class="circle"></span><span class="check"></span>'
+                                +'</label>'
+                            +'</div>'
+                        +'</td>'
+                        +'<td>'
+                            +'<div class="radio radio-primary">'
+                                +'<label>'
+                                    +'<input type="radio" name="concept' +  item.get('counter') + '_choice" id="concept' +  item.get('counter') + '_choice" value="random" checked="checked">'
+                                    +'<span class="circle"></span><span class="check"></span>'
+                                +'</label>'
+                            +'</div>'
+                        +'</td>'
+                        +'<td>'
+                            +'<div class="radio radio-primary">'
+                                +'<label>'
+                                    +'<input type="radio" name="concept' +  item.get('counter') + '_choice" id="concept' +  item.get('counter') + '_choice" value="disabled" >'
+                                    +'<span class="circle"></span><span class="check"></span>'
+                                +'</label>'
+                            +'</div>'
+                        +'</td>'
+                        +'<td>'
+                            +'<button class="btn btn-primary" type="button">Remove</button>'
+                        +'</td>'
+                    +'</tr>');
     }
   });
 
