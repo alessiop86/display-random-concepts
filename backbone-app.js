cacc5b17736905447e2cbd1a4cb282ea4fb022da
=Alessio <alessio.piergiacomi@gmail.com>
2015-02-22 14:46:45 +0100

    funzionamento con oggetti non legati al modello backbone

--- backbone-app.js
+++ backbone-app.js
@@ -157,16 +157,66 @@
 })(jQuery);
 
 
+/*var s =  new sigma({      
+        renderers: [ {
+            container: document.getElementById('innerRightCanvas'),
+            type:'canvas'   
+        }
+        ],
+        settings: {
+            sideMargin:80,
+            labelThreshold:1,
+            mouseEnabled:false,
+            enableHovering: true,
+            autoRescale: true,
+            autoResize:false,
+            rescaleIgnoreSize: true
+            
+        }
+        
+    });
+*/
+
+var  g2 = {
+  "nodes": [], /*[ {id:"ciao", "x":1,"y":1, size:2}   ],*/
+  "edges": []
+};
+
+var s2 =  new sigma(  {
+      renderers: [ {
+            container: document.getElementById('innerRightCanvas'),
+            type:'canvas'   
+        } ],
+        settings: {
+            sideMargin:80,
+            labelThreshold:1,
+            mouseEnabled:false,
+            enableHovering: true,
+            autoRescale: true,
+            autoResize:false,
+            rescaleIgnoreSize: true
+            
+        }, graph: g2
+        
+    });
+
+
 function spalaflashalert(concepts) {
     
     //pulizia
-    $("#innerRightCanvas").html("");
+    //$("#innerRightCanvas").html("");
     
+    console.log("BEFORE")
+    s2.graph.clear()
+//    console.log(g2);
+ var g2 = { }
+ g2.nodes = []
+  g2.edges = []
     //grafo vuoto
-      var g2 = {
+  /* var    g2 = {
   "nodes": [ ],
   "edges": [ ]
-};
+};*/
     
     
     var nNodes = 0;
@@ -182,9 +232,9 @@ function spalaflashalert(concepts) {
         var concept = { 
             "id" : id,
             "label" : concepts[i].get('concept'),
-            "x" : Math.round(100 * (0.5 - Math.random())),
-            "y" :  Math.round(100 * (0.5 - Math.random())),
-            'size': 1,
+            "x" : Math.round(200 * (0.5 - Math.random())), //Math.round(Math.random() * 100), //
+            "y" : Math.round(200 * (0.5 - Math.random())), //Math.round(Math.random() * 100 ), 
+            'size': Math.round(Math.random() * 6) + 2,
             'color': 'rgb('+Math.round(Math.random()*256)+','+
                   Math.round(Math.random()*256)+','+
                   Math.round(Math.random()*256)+')'
@@ -192,6 +242,7 @@ function spalaflashalert(concepts) {
             //console.log(concept)
             nNodes++;
             g2.nodes.push(concept)                
+            s2.graph.addNode(concept)
         }
     
     var nEdges = 0;
@@ -200,8 +251,16 @@ function spalaflashalert(concepts) {
         var edgesForCurrentNode = Math.round(Math.pow(Math.random(),2) * 3);
         for (var j=0; j<edgesForCurrentNode;j++) {
 
-            var start = "n" + Math.round(Math.random()*(nNodes -1));
-            var end = "n" + Math.round(Math.random()*(nNodes -1));
+            var start = g2.nodes[i].id
+            var end = start;
+            while (end == start) {            
+                end = "n" + Math.round(Math.random()*(nNodes -1));
+            }
+            
+            
+            
+            
+            
             var edge = {
               "id": "e" + nEdges,
               "source": start,
@@ -209,20 +268,116 @@ function spalaflashalert(concepts) {
             };
             nEdges++;
           //  console.log(edge)
-            g2.edges.push(edge)   
+            g2.edges.push(edge)  
+            s2.graph.addEdge(edge)
         }
 
     }
     
+   console.log("AFTER")
    console.log(g2)
-
+    /*console.log(s2)
+      
+    s2.addRenderer({
+            container: document.getElementById('innerRightCanvas'),
+            type:'canvas'   
+        });*/
+        
+   /* var s =  new sigma({      
+        renderers: [ {
+            container: document.getElementById('innerRightCanvas'),
+            type:'canvas'   
+        }
+        ],
+        graph: g2,
+        settings: {
+            sideMargin:80,
+            labelThreshold:1,
+            mouseEnabled:false,
+            enableHovering: true,
+            autoRescale: true,
+            autoResize:false,
+            rescaleIgnoreSize: true
+            
+        }
+        
+    });*/
     
-    var s  = new sigma({
+    
+   //s.graph(g2);
+    
+    /*var s  = new sigma({
       graph: g2,
-      container: 'innerRightCanvas'
-    });
+        type:'canvas',
+      container: 'innerRightCanvas',
+        settings: {
+            sideMargin:80,
+            labelThreshold:1,
+            mouseEnabled:false,
+            enableHovering: true,
+            autoRescale: true,
+            autoResize:false,
+            rescaleIgnoreSize: true
+            
+        }
+        
+    });*/
+    //Singleton.getInstance(g2);
 
+    //s2.refresh()
+   
+   //s2.graph.addNode({id:"ciao", "x":1,"y":1, size:2})
+   //s2.graph.addNode({id:"ciao2", "x":3,"y":3, size:2})
+   s2.refresh()
 
   
     
 }
+
+var Singleton = (function () {
+    var instance;
+ 
+    function createInstance(g2) {
+        
+         var s =  new sigma({      
+        renderers: [ {
+            container: document.getElementById('innerRightCanvas'),
+            type:'canvas'   
+        }
+        ],
+        graph: g2,
+        settings: {
+            sideMargin:80,
+            labelThreshold:1,
+            mouseEnabled:false,
+            enableHovering: true,
+            autoRescale: true,
+            autoResize:false,
+            rescaleIgnoreSize: true
+            
+        }
+        
+        });
+        return s;
+    }
+ 
+    return {
+        getInstance: function (g2) {
+            if (!instance) {
+                console.log("Create")
+               // instance = createInstance(g2);
+            }
+            else {
+                console.log("Do nothing")
+                //instance.kill()
+                //instance = createInstance(g2);
+                //console.log(instance.graph.nodes)
+                //instance.graph.nodes(g2.nodes)
+                //console.log(g2)                
+                instance.refresh()
+                
+            }
+            return instance;
+        }
+    };
+})();
